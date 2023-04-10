3daada9852645b5b78755384c939916b9bc4baf3
vansilich <kulpovvvan@gmail.com>
2023-04-10 09:55:14 +0300

    Add SearchHints handler props

--- components/searchElement/SearchInput.tsx
+++ components/searchElement/SearchInput.tsx
@@ -1,17 +1,26 @@
 import { FC } from "react";
 import Button from "../buttons/Button";
+import { FormEventHandler } from "react";
 
 interface SearchInputProps {
-    
+    handleUserType: FormEventHandler<HTMLInputElement>,
+    handleSearch: FormEventHandler<HTMLFormElement>,
 }
  
-const SearchInput: FC<SearchInputProps> = () => {
-    return <>
-            <form className=" d-flex my-2">
-                <input className="input-find" type="search" placeholder="Поиск" aria-label="Search" />
-                <Button btn_style="find-blue">Найти</Button>
-            </form>
-    </>;
+const SearchInput: FC<SearchInputProps> = ({handleSearch, handleUserType}) => {
+    
+    return (
+        <form className=" d-flex my-2" onSubmit={handleSearch}>
+            <input 
+                onInput={handleUserType}
+                className="input-find" 
+                type="search" 
+                placeholder="Поиск" 
+                aria-label="Search"
+            />
+            <Button btn_style="find-blue">Найти</Button>
+        </form>
+    );
 }
  
 export default SearchInput;
