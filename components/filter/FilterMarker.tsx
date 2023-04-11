1cf06af44131a3cd1cb74de07083133d73006768
Ваше Имя <picus.2022@mail.ru>
2023-04-11 16:34:39 +0300

    antd checkbox

--- components/filter/FilterMarker.tsx
+++ components/filter/FilterMarker.tsx
@@ -1,6 +1,7 @@
 import { FC, ReactNode } from "react";
 import { MdCancel, MdOutlineSegment, MdUnfoldMore } from "react-icons/md";
 import Input from "../inputs/Input";
+import { Checkbox } from "antd";
 
 interface FilterMarkerProps {
     title: string,
@@ -11,7 +12,8 @@ interface FilterMarkerProps {
 
 const FilterMarker: FC<FilterMarkerProps> = ({ title, isGray = false, isActive = false, onChange }) => {
     return <label className={'filter-marker ' + (isActive ? 'filter-input-active ' : '') + (isGray ? 'input-bg-gray' : '')}>
-        <input className='square-gray ' type="checkbox" checked={isActive} onChange={e => onChange(e.target.checked)}/>
+        <Checkbox checked={isActive} onChange={e => onChange(e.target.checked)} />
+        {/* <input className='square-gray ' type="checkbox" checked={isActive} onChange={e => onChange(e.target.checked)}/> */}
         <span className="font-size-16-gray text-overflow">{title}</span>
     </label>;
 }
