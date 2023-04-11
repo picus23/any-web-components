c573ee40973180c88c66493e1e0224da5c239346
Ваше Имя <picus.2022@mail.ru>
2023-04-11 13:22:22 +0300

    FilterFieldWrapper

---
+++ components/filter/FilterFieldWrapper.tsx
@@ -0,0 +1,14 @@
+import React, { FC, ReactNode } from "react";
+
+interface FilterFieldWrapperProps {
+    children: ReactNode;
+    height?: number, 
+}
+ 
+const FilterFieldWrapper: FC<FilterFieldWrapperProps> = ({children, height = 600}) => {
+    return <ul className="list-group list-group-flush py-2 overflow-auto" style={{ maxHeight: height, minHeight: height }}>
+        {children}
+    </ul>;
+}
+ 
+export default FilterFieldWrapper;
