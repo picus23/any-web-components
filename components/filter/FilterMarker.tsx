951ce75cca32c4a79d7cc4a46f2c775d77cde007
Ваше Имя <picus.2022@mail.ru>
2023-04-11 16:32:10 +0300

    FilterMarker change

--- components/filter/FilterMarker.tsx
+++ components/filter/FilterMarker.tsx
@@ -3,16 +3,15 @@ import { MdCancel, MdOutlineSegment, MdUnfoldMore } from "react-icons/md";
 import Input from "../inputs/Input";
 
 interface FilterMarkerProps {
-    title?: string,
+    title: string,
     isActive?: boolean,
     isGray?: boolean,
-    onClick?: () => void,
+    onChange?: (isChecked: boolean) => void,
 }
 
-const FilterMarker: FC<FilterMarkerProps> = ({ title, isGray = false, isActive = false, onClick }) => {
+const FilterMarker: FC<FilterMarkerProps> = ({ title, isGray = false, isActive = false, onChange }) => {
     return <label className={'filter-marker ' + (isActive ? 'filter-input-active ' : '') + (isGray ? 'input-bg-gray' : '')}>
-        <input className='square-gray ' type="checkbox" onClick={onClick}/>
-        {/* <input className='square-gray ' type="checkbox" (isActive ? checked : false) /> */}
+        <input className='square-gray ' type="checkbox" checked={isActive} onChange={e => onChange(e.target.checked)}/>
         <span className="font-size-16-gray text-overflow">{title}</span>
     </label>;
 }
