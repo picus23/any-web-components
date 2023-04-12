56a7d1e30b16386165c1d7bcbacb17051587b884
Ваше Имя <picus.2022@mail.ru>
2023-04-12 09:05:28 +0300

    FilterButtonShow

--- components/filter/FilterButtonShow.tsx
+++ components/filter/FilterButtonShow.tsx
@@ -6,19 +6,14 @@ interface FilterButtonShowProps {
     icon?: ReactNode,
     icon2?: ReactNode
     counter?: number,
-    text_style?: string,
-    counter_style?: string,
-    title?: string,
+    onClick?: () => void,
 }
-
-function FilterButtonShow(props: FilterButtonShowProps) {
-    return <button className='filter-btn-show mt-2 mb-2'>
-        <span className='font-size-16-black fw-500'>{props.title}</span>
-        <span className="filter-counter-up">{props.counter ?? 0}</span>
+ 
+const FilterButtonShow: FC<FilterButtonShowProps> = ({children, icon, icon2, counter, onClick}) => {
+    return <button className='filter-btn-show mt-2 mb-2' onClick={onClick}>
+        <span className='font-size-16-black fw-500'>{children}</span>
+        { counter && <span className="filter-counter-up">{counter}</span> }
     </button>
 }
-
+ 
 export default FilterButtonShow;
-
-
-// {`${btn_style ? 'my-btn-' + btn_style : 'btn-primary'}`} 
