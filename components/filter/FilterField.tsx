2fbc319b24ae7276e0803b71621f027205943732
Ваше Имя <picus.2022@mail.ru>
2023-04-17 14:25:04 +0300

    Filters change

--- components/filter/FilterField.tsx
+++ components/filter/FilterField.tsx
@@ -9,7 +9,7 @@ interface FilterFieldProps {
 
     isActive?: boolean,
     isSelected?: boolean,
-    isOpen?: boolean,
+    isOpen?: false | 'open' | 'close',
     isEmbedded?: boolean,
 
 }
@@ -21,15 +21,17 @@ const FilterField: FC<FilterFieldProps> = ({ children, title, onClick, isActive 
             className={
                 'filter-field my-1 ' +
                 (isSelected ? 'selected ' : '') +
-                (isActive ? 'active ' : '') +
-                (isOpen ? ' filter-field-child ' : '')
+                (isActive ? 'active ' : '') + 
+                (isOpen=='open' ? 'isOpen ' : '')
             }
             onClick={onClick}>
             <div className="d-flex align-items-center">
                 {isActive ? <img src="/kit/white_square.png" alt="" /> : <img src="/kit/gray_square.png" alt="" />}
                 <span className="fw-500 ms-2 d-flex text-left">{title}</span>
             </div>
-            <MdChevronRight size={24} fill={isActive ? 'white' : 'gray'} />
+                <div className={` ${isOpen=='close' ? 'filter-field-up':''} ${isOpen=='open' ? 'filter-field-down':''} `}>
+                    <MdChevronRight size={24} fill={isActive ? 'white' : 'gray'} />
+                </div>
             </button>
     </li >
 }
--- components/filter/FilterMarkerWrapper.tsx
+++ components/filter/FilterMarkerWrapper.tsx
@@ -5,7 +5,7 @@ interface FilterMarkerWrapperProps {
 }
 
 const FilterMarkerWrapper: FC<FilterMarkerWrapperProps> = ({ children }) => {
-    return <div className="filter-chips-block">
+    return <div className="filter-chips-block py-2">
         {children}
     </div>
 }
