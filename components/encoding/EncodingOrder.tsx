c3ed907c7a15a709ca9fe0be947ba768e49333eb
Ваше Имя <picus.2022@mail.ru>
2023-05-04 08:36:21 +0300

    input.radio set default checkbox

--- components/encoding/EncodingOrder.tsx
+++ components/encoding/EncodingOrder.tsx
@@ -5,13 +5,10 @@ interface EncodingOrderProps {
     title?: string,
     subtitle?: string,
     value?: number,
+    checked?: boolean,
 }
 
-<<<<<<< HEAD
-const EncodingOrder: FC<EncodingOrderProps> = ({ title, subtitle, value }) => {
-=======
 const EncodingOrder: FC<EncodingOrderProps> = ({ title, subtitle, value, checked = false }) => {
->>>>>>> 57a59e5e3c91ef01670f985529a923fd580c906c
     return <>
         <Radio className='d-flex gap-2 rounded-2 p-2 my-1 order '
             value={value} name='1' defaultChecked={checked}>
