53f9e3ce04673eaa04e880e4470ce54e89b1d9c2
Ваше Имя <picus.2022@mail.ru>
2023-05-10 11:04:50 +0300

    Добавил ховер на чекбокс и состояние isSelected

--- components/filter/FilterMarker.tsx
+++ components/filter/FilterMarker.tsx
@@ -7,15 +7,25 @@ interface FilterMarkerProps {
     title: string | ReactNode,
     isActive?: boolean,
     isGray?: boolean,
+    isSelected?: boolean,
     color?: string,
     onChange?: (isChecked: boolean) => void,
+    onHover?: (isOver: boolean) => void,
 }
 
-const FilterMarker: FC<FilterMarkerProps> = ({ title, isGray = false, isActive = false, onChange, color }) => {
-    return <label className={'filter-marker ' + (isActive ? 'filter-input-active ' : '') + (isGray ? 'input-bg-gray' : '')}>
-        <Checkbox checked={isActive} onChange={e => onChange(e.target.checked)} />
-        {/* <input className='square-gray ' type="checkbox" checked={isActive} onChange={e => onChange(e.target.checked)}/> */}
-        <span className="font-size-16-gray text-overflow" style={{color}}>{title}</span>
+const FilterMarker: FC<FilterMarkerProps> = ({ title, isGray = false, isActive = false, isSelected = false, onChange, onHover, color }) => {
+    return <label 
+        className={'filter-marker ' + 
+        (isActive ? 'filter-input-active ' : '') 
+        + (isGray ? 'input-bg-gray ' : '')
+        + (isSelected ? 'bg-primary bg-opacity-10 ' : '')
+        + (!isActive && isSelected ? 'border border-success ' : '')
+        }
+        onMouseLeave={() => onHover && onHover(false)}
+        onMouseOver={() => onHover && onHover(true)}
+    >
+            <Checkbox checked={isActive} onChange={e => onChange && onChange(e.target.checked)} />
+        <span className="font-size-16-gray text-overflow" style={{ color }}>{title}</span>
     </label>;
 }
 
