e0a9054689b24c6909bab126b04ef57571d4b848
Misc1170 <sloopy1170@gmail.com>
2023-05-12 17:35:29 +0300

    add graph (begin)

--- pages/slider/index.tsx
+++ pages/slider/index.tsx
@@ -4,29 +4,28 @@ import Image from "next/image";
 import Slider from "@/components/slider/Slider";
 import Graph from "@/components/slider/Graph";
 import { LineChart, Line, CartesianGrid, XAxis, YAxis } from "recharts";
-import { VictoryChart,VictoryLine } from "victory";
+import { VictoryChart, VictoryLine } from "victory";
 
-const values = [0.0111, 5, 12, 20, 100];
+const values = [0.0111, 5, 12, 20, 30, 100];
 const data = [
     {
-        name: values[0],
+        name: 1,
         cv: values[0],
-        name1: values[0],
     },
     {
-        name: values[1],
+        name: 4,
         cv: values[1],
     },
     {
-        name: values[2],
+        name: 7,
         cv: values[2],
     },
     {
-        name: values[3],
-        cv: values[3],
+        name: 10,
+        cv: 12,
     },
     {
-        name: values[4],
+        name: 14,
         cv: values[4],
     },
 ];
@@ -52,8 +51,8 @@ export default function () {
             <LineChart width={600} height={300} data={data}>
                 <Line type="monotone" dataKey="cv" stroke="#0085FF" />
                 <CartesianGrid stroke="#ccc" />
-                <XAxis hide dataKey=" " /> Ошибка из за hide
-                <YAxis name="" />
+                <XAxis dataKey=" " /> {/* Ошибка из за hide,если есть. Она делает чистой X */}
+                <YAxis tickCount={values.length} />
             </LineChart>
 
             {/* <Slider valuesCv={values} minValue={1} maxValue={20}/> */}
