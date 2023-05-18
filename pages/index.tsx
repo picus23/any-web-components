268531de73af983162cafee766fd3c18a60a8976
Ваше Имя <picus.2022@mail.ru>
2023-05-18 12:13:54 +0300

    почистил зависимости

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,40 +5,18 @@ import CartComponent from "./CartComponent";
 
 export default function Index() {
   return <div className="p-2">
-    components galery
-    <hr />
 
-
-
-
-
-    <Link href="/buttons">Кнопочки</Link>
-    <br />
     <CartComponent link="/slider" title="Слайдер" description="" />
     <CartComponent link="/navbar" title="навигатор" description="" />
-    <CartComponent link="/search" title="Подсказки для поиска" description="Комплекс компонентов специально разработаных для упрощения отображений подсказок поиска" />
-    <CartComponent link="/footer" title="Footer" description="Комплекс компонентов специально разработаных для упрощения отображений" />
-    <CartComponent link="/basket" title="Корзина" description="Комплекс компонентов специально разработаных для упрощения отображений" />
-    <CartComponent link="/footer" title="Подвал" description="Комплекс компонентов специально разработаных для упрощения отображений" />
-    <CartComponent link="/tableRow" title="Table Row" description="Комплекс компонентов специально разработаных для упрощения отображений" />
-    <CartComponent link="/filter" title="Filter" description="Комплекс компонентов специально разработаных для упрощения отображений" />
+    <CartComponent link="/search" title="Подсказки для поиска" description="* подсказок поиска" />
+    <CartComponent link="/footer" title="Footer" description="*" />
+    <CartComponent link="/basket" title="Корзина" description="*" />
+    <CartComponent link="/footer" title="Подвал" description="*" />
+    <CartComponent link="/tableRow" title="Table Row" description="*" />
+    <CartComponent link="/filter" title="Filter" description="*" />
     <CartComponent link="/popularFilter" title="Popular Filter" description="ну нафиг" />
     <CartComponent link="/encoding" title="Кодировки" description="" />
 
-
-    <br />
-
-    <CartComponent link="/cartItem" title="Карта товара" description="" />
-
-    {/* <Link href="/cartItem">Карта товара</Link> */}
-    {/* <Link href="/tableRow">TableRow</Link> */}
-    <br />
-    <Link href="/tableCell">Ячейки таблицы</Link>
-    <br />
-    <Link href="/catalogMenu">Каталог Меню</Link>
-    <br />
-    <Link href="/header">Шапка с поиском</Link>
-    <br />
   </div>
 }
 
