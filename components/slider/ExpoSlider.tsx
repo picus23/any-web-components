538956f169beee8ebcc4ccf81859fc8732456c24
Misc1170 <sloopy1170@gmail.com>
2023-05-18 17:30:00 +0300

    edit value slider

---
+++ components/slider/ExpoSlider.tsx
@@ -0,0 +1,173 @@
+import { FC, ReactNode, useState } from "react";
+import { Slider as SliderA } from "antd";
+import SliderValueRange from "./SliderValueRange";
+
+interface ExpoSliderProps {
+    valuesCv: number[],
+    minPropValue?: number,
+    maxPropValue?: number,
+    onChange?: (target: [min: number, max: number]) => void,
+    format?: (value: number) => string | ReactNode,
+}
+
+interface iChangeKeys {
+    [key: number]: number,
+}
+interface iChangeKeysEmpty {
+    [key: string]: string,
+}
+
+const ExpoSlider: FC<ExpoSliderProps> = ({ valuesCv, minPropValue, maxPropValue, format, onChange }) => {
+    const minValue = Math.min(...valuesCv);
+    const maxValue = Math.max(...valuesCv);
+
+    let minDisplayValue = useState(minValue);
+    let maxDisplayValue = useState(maxValue);
+
+
+    // Делю массив на разряды десяток
+    let countRank = 0;
+    if (maxValue.toString().length > 2) {
+        countRank = maxValue.toString().length - 1
+    } else {
+        countRank = maxValue.toString().length
+    }
+
+    console.log(valuesCv)
+    let rankValueCv = new Array(countRank);
+    let prev = 0;
+    let curr = 10;
+    var lenghtPrevArr = 0;
+    var tempLenght = 0;
+    for (let i = 0; i < countRank; i++) {
+        let position = 0;
+        rankValueCv[i] = [];
+        if (i > 0) {
+            prev = curr;
+            curr = String(curr);
+            curr += '0';
+            curr = Number(curr);
+
+        }
+        for (let j = tempLenght; j < valuesCv.length; j++) {
+
+            if (valuesCv[j] >= prev && valuesCv[j] <= curr) {
+                rankValueCv[i][position] = valuesCv[j];
+                lenghtPrevArr = position;
+                position++;
+            } else {
+                tempLenght += ++lenghtPrevArr;
+                break;
+            }
+
+        }
+    }
+    console.log('Дроблённый массив на разряды', rankValueCv)
+    // Делю массив на разряды десяток
+
+    let changeKeys: iChangeKeys = {};
+    const changeKeysEmpty: iChangeKeysEmpty = {};
+
+    let maxRange: number = 0
+
+    let prevValue = 0;
+    rankValueCv.map((array, index) => {
+
+        let rangeEverySubArray = (maxValue / rankValueCv.length) * (index + 1)
+        if (index == 0) {
+            var firstValue = rangeEverySubArray / 3;
+        } else {
+            firstValue = 0;
+        }
+
+        array.map(item => {
+            if (firstValue != 0) {
+                changeKeys[firstValue] = item;
+                changeKeysEmpty[firstValue] = ' ';
+                prevValue = firstValue;
+                firstValue = 0;
+            } else {
+                maxRange = ((rangeEverySubArray - prevValue) / 2) + prevValue;
+                prevValue = maxRange;
+                changeKeys[maxRange] = item;
+                changeKeysEmpty[maxRange] = ' ';
+            }
+        })
+    })
+    console.log('Change Keys', changeKeys)
+
+    const formatter = (value: number) => {
+        return changeKeys[value];
+    };
+
+    const change = (target: [number, number]) => {
+        onChange([
+            formatter(target[0]),
+            formatter(target[1])
+        ]);
+    }
+
+    return <>
+        <SliderA
+            range
+            marks={changeKeysEmpty}
+            defaultValue={[minValue ?? minPropValue, maxRange ?? maxPropValue]}
+            // min={minValueValue}
+            max={maxRange}
+            onChange={change}
+            tooltip={{ formatter: (value?: number) => format ? format(formatter(value!)) : formatter(value!) }}
+            step={null}
+        />
+
+        <div className="d-flex justify-content-between">
+            <SliderValueRange
+                value={minDisplayValue}
+                unit={'cv'}
+            />
+            <SliderValueRange
+                value={maxDisplayValue}
+                unit={'cv'}
+            />
+        </div>
+    </>
+}
+
+export default ExpoSlider;
+
+
+
+    //     array.map((item, index) => {
+
+    //         if (index === 0) {
+    //             let firstValue = ((rangeEverySubArray / 3) * (index + 1)) //1333 | 2666 | 4000
+    //             // let firstValue = ((rangeEverySubArray / 3) * (index + 1)) * 2 //1333 | 2666 | 4000
+    //             maxRange = firstValue;
+    //             prevValue = maxRange;
+    //             console.log('First maxRange', maxRange, index)
+
+    //             changeKeys[maxRange] = item;
+    //             changeKeysEmpty[maxRange] = ' ';
+
+    //             console.log('First Range', maxRange)
+
+    //         } else {
+
+    //             maxRange = ((rangeEverySubArray - prevValue) / 2) + prevValue;
+    //             console.log('Other Range', maxRange)
+
+    //             changeKeys[maxRange] = item;
+    //             changeKeysEmpty[maxRange] = ' ';
+
+    //         }
+    //     })
+    // })
+
+    // let changeKeys : iChangeKeys = {};
+    // const changeKeysEmpty : iChangeKeysEmpty = {};
+
+    // let maxRange: number = 0
+    // valuesCv.map((itm, index) => {
+    //     maxRange = Math.round(distanceBetweenValueOnSlider * index);
+    //     changeKeys[maxRange] = itm;
+    //     changeKeysEmpty[maxRange] = ' ';
+    // })
--- pages/slider/index.tsx
+++ pages/slider/index.tsx
@@ -3,59 +3,54 @@ import Link from "next/link";
 import Image from "next/image";
 import Slider from "@/components/slider/Slider";
 import { LineChart, Line, CartesianGrid, XAxis, YAxis } from "recharts";
+import ExpoSlider from "@/components/slider/ExpoSlider";
+import { Tooltip } from "antd";
 
-const values = [1, 5, 12, 20,22, 30, 100];
+const values = [2,4,6,10,20,40,100,200,400,1000,5000,10000];
 const sortedArray = values.sort((a, b) => a - b);
 const minValue = Math.min(...sortedArray);
 const maxValue = Math.max(...sortedArray);
 
 let data = [
     {
-        value : values[0],
+        value: values[0],
     },
     {
-        value : values[1],
+        value: values[1],
     },
     {
-        value : values[2],
+        value: values[2],
     },
     {
-        value : values[3],
+        value: values[3],
     },
     {
-        value : values[4],
+        value: values[4],
     },
     {
-        value : values[5],
+        value: values[5],
     },
     {
-        value : values[6],
+        value: values[6],
+    },
+    {
+        value: values[7],
+    },
+    {
+        value: values[8],
+    },
+    {
+        value: values[9],
+    },
+    {
+        value: values[10],
+    },
+    {
+        value: values[11],
     },
 ];
-// values.map(el=> {
-//     data[value] = el;
-// })
-console.log(data)
-// const data = [
-//     {
-//         unit: values[0],
-//     },
-//     {
-//         unit: values[1],
-//     },
-//     {
-//         unit: values[2],
-//     },
-//     {
-//         unit: values[3]
-//     },
-//     {
-//         unit: values[4],
-//     },
-//     {
-//         unit: values[5],
-//     },
-// ];
+console.log(data);
+console.log(sortedArray.length);
 
 
 export default function () {
@@ -74,47 +69,36 @@ export default function () {
         <hr />
 
         <h1>МОЙ</h1>
-        <div className="container-xxl" style={{ outline: '1px solid #000' }}>
+        <div className="container-xxl p-0" style={{ outline: '1px solid #000',width:'1000px'}}>
 
-            <LineChart 
-            width={1100} 
-            height={300} 
-            margin={{
-                top:20,
-                right:20,
-                left:20,
-            }} 
-            data={data}>
+            <LineChart
+                width={1000}
+                height={300}
+                margin={{
+                    top: 20,
+                    right: 20,
+                    left: 20,
+                }}
+                data={data}
+            >
 
                 <Line type="monotone" dataKey="value" stroke="#0085FF" />
                 <CartesianGrid stroke="#ccc" />
-                <XAxis dataKey=" " /> {/* Ошибка из за hide,если есть. Она делает чистой X */}
-                {/* <XAxis dataKey="unit" name=" " />*/}
-                <YAxis dataKey="value" domain={[minValue,maxValue]} tickCount={sortedArray.length} />
-                {/* <YAxis dataKey="unit" domain={sortedArray} /> */}
+                <XAxis dataKey="value" /> 
+                <YAxis domain={[0,10000]} tickCount={10}/>
 
             </LineChart>
 
-            {/* <Slider valuesCv={values} minValue={1} maxValue={20}/> */}
-            {/* <Slider valuesCv={values} format={(value) => `${value} cv`} /> */}
+            <ExpoSlider
+                valuesCv={sortedArray}
+                onChange={() => { }}
+            />
 
-            {/* <VictoryChart
-                theme={VictoryTheme.material}
-            >
-                <VictoryLine
-                    style={{
-                        data: { stroke: "#0085FF" },
-                        parent: { border: "1px solid #ccc" }
-                    }}
-                    data={[
-                        { x: 1, y: values[0] },
-                        { x: 2, y: values[1] },
-                        { x: 3, y: 5 },
-                        { x: 4, y: 4 },
-                        { x: '', y: 7 }
-                    ]}
-                />
-            </VictoryChart> */}
+            {/* <Slider
+             valuesCv={values}
+              format={(value) => `${value} cv`} 
+              onChange={()=>{}}
+              /> */}
         </div>
 
         <hr />
