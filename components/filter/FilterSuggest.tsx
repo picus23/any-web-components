ead475c616dc6dacd53ced599971f909211f41ae
Ваше Имя <picus.2022@mail.ru>
2023-06-15 10:48:11 +0300

    добавил border + margin

--- components/filter/FilterSuggest.tsx
+++ components/filter/FilterSuggest.tsx
@@ -10,14 +10,17 @@ interface FilterSuggestProps {
 }
 
 const FilterSuggest: FC<FilterSuggestProps> = ({ title, icon, isSuggest = false, onClick }) => {
-    return <button className='d-flex align-items-center justify-content-center border-0 rounded-pill gap-2 px-2 py-1 position-relative'
+    return <button className='d-flex align-items-center justify-content-center rounded-pill gap-1 px-2 py-1 position-relative border border-secondary-subtle me-1'
         style={{ background: '#fff' }} onClick={onClick}>
         {icon}
         <span className="font-size-16-gray text-overflow">{title}</span>
-        {isSuggest ?
-            <span className="position-absolute top-0 start-100 translate-middle p-2 bg-primary border border-primary rounded-circle">
-                <span className="visually-hidden">New alerts</span>
-            </span> : false}
+        {
+            isSuggest ?
+                <span className="position-absolute top-0 start-100 translate-middle p-2 bg-primary border border-primary rounded-circle">
+                    <span className="visually-hidden">New alerts</span>
+                </span>
+                : false
+        }
     </button>;
 }
 
