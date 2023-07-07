bbe5b349276022ea73af6cebbab5cd708d5d8ca2
Ваше Имя <picus.2022@mail.ru>
2023-07-07 14:41:52 +0300

    Обновил список Explorer

--- components/explorer/ExplorerItem.tsx
+++ components/explorer/ExplorerItem.tsx
@@ -1,5 +1,5 @@
 import { FC } from "react";
-import { MdArrowForwardIos, MdCheck } from "react-icons/md";
+import { MdArrowForwardIos, MdCheck, MdCheckBoxOutlineBlank } from "react-icons/md";
 import ExplorerButton from './ExplorerButton'
 
 interface ExplorerItemProps {
@@ -11,16 +11,49 @@ interface ExplorerItemProps {
 }
 
 const ExplorerItem: FC<ExplorerItemProps> = ({ title, checked, isFolder, onChange, onOpen }) => {
+
+
+    if (isFolder) {
+        return <li tabIndex={0} className={`ExplorerItem  ${checked && 'active'}`} >
+            <div className='ExplorerItemCheckbox hoverIcon' onClick={() => onChange(checked != 0 ? false : true)}>
+                <div className="icon">
+                    {
+                        checked == 0
+                            ? <MdCheckBoxOutlineBlank size={22} color="#96969660" />
+                            : <MdCheck size={22} color={checked == 2 ? '#0085FF' : '#969696'} />
+                    }
+                </div>
+            </div>
+            <div className={`ExplorerItemTitle folder`} onClick={() => { isFolder && onOpen() }}>
+                <div className="title">
+                    {title}
+                </div>
+                <div className="folderArrow">
+                    <MdArrowForwardIos size={18} />
+                </div>
+            </div>
+
+        </li>
+    }
+
+
+
     return <li tabIndex={0} className={`ExplorerItem  ${checked && 'active'}`} onClick={() => onChange(checked != 0 ? false : true)}>
-        <div className='ExplorerItemCheckbox'>
-            <MdCheck size={22} color={checked == 2 ? '#0085FF' : '#969696'} />
+        <div className='ExplorerItemCheckbox' >
+            <div className="icon">
+                {
+                    checked == 0
+                        ? <MdCheckBoxOutlineBlank size={22} color="#96969660" />
+                        : <MdCheck size={22} color={checked == 2 ? '#0085FF' : '#969696'} />
+                }
+            </div>
         </div>
         <div className="ExplorerItemTitle">
-            {title}
+            <div className="title">
+                {title}
+            </div>
         </div>
-        {
-            isFolder && <ExplorerButton onClick={onOpen}/>
-        }
+
     </li>
 }
 
