99e2a0135c168852014319c4fbddd4d4e05151ae
Ваше Имя <picus.2022@mail.ru>
2023-07-13 10:18:23 +0300

    Добавил новый компонент для вывода кодировки в текстовых подсказках

---
+++ components/searchElement/fields/FieldPagetitle.tsx
@@ -0,0 +1,28 @@
+import Image from 'next/image';
+import ButtonGrayArrow from '../../buttons/ButtonGrayArrow';
+import { FC, ReactNode } from 'react'
+
+
+interface FieldPagetitleProps {
+    pagetitle: ReactNode,
+    imageUrl: string,
+    basketButton: ReactNode,
+    handleArrowClick?: () => void,
+}
+
+
+const FieldPagetitle: FC<FieldPagetitleProps> = ({ pagetitle, imageUrl, basketButton, handleArrowClick }) => {
+
+    return <div className="d-flex gap-2 align-items-center">
+        <Image src={imageUrl} width={64} height={64} alt='product' />
+        <div className="d-flex align-items-center justify-content-between flex-grow-1">
+            {pagetitle}
+            {basketButton}
+        </div>
+        <div>
+            <ButtonGrayArrow onClick={handleArrowClick} />
+        </div>
+    </div>
+}
+export default FieldPagetitle;
+
