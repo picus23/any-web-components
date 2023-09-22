a932263b202c7bc51f68a7c75d1d6d8f03f2b561
Ваше Имя <picus.2022@mail.ru>
2023-09-22 11:19:48 +0300

    Поправил маркеры

--- components/Marker/Marker.tsx
+++ components/Marker/Marker.tsx
@@ -1,18 +1,21 @@
+import { Tooltip } from "antd"
 import { Context, FC, useContext } from "react"
 import { MdCancel } from "react-icons/md"
 
 
 
 interface MarkerProps {
+    description: string,
     title: string,
     onClick?: () => void,
     onClose: () => void,
 }
 
 
-const Marker: FC<MarkerProps> = ({ title, onClick, onClose }) => {
+const Marker: FC<MarkerProps> = ({ description, title, onClick, onClose }) => {
 
-    return <button
+    return <Tooltip title={description} placement="bottom">
+        <button
             className={`btn marker ${false ? 'btn-primary' : 'btn-outline-secondary'} d-inline-block me-1 px-3 btn-sm rounded-5 mb-1 bg-info-subtle text-emphasis-info`}
         >
             <span className="marker-title text-nowrap" onClick={onClick}>
@@ -20,9 +23,10 @@ const Marker: FC<MarkerProps> = ({ title, onClick, onClose }) => {
             </span>
 
             <span onClick={onClose}>
-                <MdCancel className="d-inline-block ms-1"/>
+                <MdCancel className="d-inline-block ms-1" />
             </span>
         </button>
+    </Tooltip>
 }
 
 export default Marker
